2c765de392a669ea53fb6f9b42b9c193cb2fc485
jaredDelao <jaredhm07@gmail.com>
2021-06-29 17:21:22 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+const titulo = document.getElementById('titulo');
+const descripcion = document.getElementById('descripcion');
+
+window.addEventListener("load", (event) => {
+  console.log({ params });
+  console.log("page is fully loaded");
+
+  if (params.nbResponse === 'Aprobado') {
+    //   Mostrar landing aprobado
+    titulo.innerText = params.nbResponse;
+    // descripcion.innerText = params.nb_error;
+} else {
+    //   Mostrar landing error
+    titulo.innerText = params.nbResponse;
+    descripcion.innerText = params.nb_error;
+  }
+});
